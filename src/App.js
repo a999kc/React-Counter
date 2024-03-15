f526bc435878aa4fa7e5e5b4d07b0c0416713c34
Andrey Moskokov <asm2906@gmail.com>
2024-03-15 10:01:44 +0300

    1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
+  
+  const [count,setCount] = React.useState(0)
+
+  const onClickPlus = () => {
+    setCount(count+1);
+  }
+  const onClickMinus = () => {
+    setCount(count-1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='main-div'>
+        <h2 className="header">
+          Счетчик:
+        </h2>
+        <span className='counter'>
+          {count}
+          
+        </span>
+
+        <div className="btn-div">
+          <button onClick={onClickPlus} className='plus'>
+            Плюс
+          </button>
+
+          <button onClick={onClickMinus} className='minus'>
+            Минус
+          </button>
+        </div>
+      </div>
+
+    </>
+
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
-export default App;
+
